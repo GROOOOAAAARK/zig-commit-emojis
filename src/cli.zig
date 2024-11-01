0a30e865886da60a8f6c2d153dc73eeab13b0cec
GROOOOAAAARK <[email protected]>
2024-11-01 17:06:38 +0100

    feat: compare description and search in a common lower case format

--- src/cli.zig
+++ src/cli.zig
@@ -13,8 +13,15 @@ var args_definition = struct {
 fn contains_subsequence(haystack: []const u8, needle: []const u8) bool {
     const needle_len = needle.len;
     const haystack_len = haystack.len;
+    if (needle_len > haystack_len) {
+        return false;
+    }
+    var buf1: [1024]u8 = undefined;
+    var buf2: [1024]u8 = undefined;
+    const lower_needle = std.ascii.lowerString(&buf1, needle);
     for (0..haystack_len - needle_len - 1) |i| {
-        if (std.mem.eql(u8, haystack[i .. i + needle_len], needle)) {
+        const lower_haystack = std.ascii.lowerString(&buf2, haystack[i .. i + needle_len]);
+        if (std.mem.eql(u8, lower_haystack, lower_needle)) {
             return true;
         }
     }
