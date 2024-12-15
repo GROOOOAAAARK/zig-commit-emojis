5a3bdfe30eb4daad527ca703ff2983e7448501c9
GROOOOAAAARK <t01_n10@protonmail.com>
2024-12-15 13:37:36 +0100

    fix: update search logic to allow effective search on the last word

--- src/cli.zig
+++ src/cli.zig
@@ -19,7 +19,7 @@ fn contains_subsequence(haystack: []const u8, needle: []const u8) bool {
     var buf1: [1024]u8 = undefined;
     var buf2: [1024]u8 = undefined;
     const lower_needle = std.ascii.lowerString(&buf1, needle);
-    for (0..haystack_len - needle_len - 1) |i| {
+    for (0..haystack_len - needle_len + 1) |i| {
         const lower_haystack = std.ascii.lowerString(&buf2, haystack[i .. i + needle_len]);
         if (std.mem.eql(u8, lower_haystack, lower_needle)) {
             return true;
