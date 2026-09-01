0329858e029d95bec678ec3ff11d089a9130ff67
GRK <gitkey.aluminum361@passmail.net>
2026-09-01 23:20:15 +0200

    refactor: move search utils to dedicated file

---
+++ src/search_utils.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+
+fn contains_subsequence(haystack: []const u8, needle: []const u8) bool {
+    const needle_len = needle.len;
+    const haystack_len = haystack.len;
+    if (needle_len > haystack_len) {
+        return false;
+    }
+    var buf1: [1024]u8 = undefined;
+    var buf2: [1024]u8 = undefined;
+    const lower_needle = std.ascii.lowerString(&buf1, needle);
+    for (0..haystack_len - needle_len + 1) |i| {
+        const lower_haystack = std.ascii.lowerString(&buf2, haystack[i .. i + needle_len]);
+        if (std.mem.eql(u8, lower_haystack, lower_needle)) {
+            return true;
+        }
+    }
+    return false;
+}
